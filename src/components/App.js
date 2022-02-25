c96d4e10db55f53ba38707dff4c62e5f9e47f3d7
田中修平 <tanakashuuhei@tanakashuuheinoMacBook-Pro.local>
2022-02-25 16:11:51 +0900

    createsetname

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React , { useState } from 'react';
 import SignIn from './SignIn';
 
 export default () => {
+  const [name, setName] = useState('');
+  console.log({name});
   return <SignIn />;
 };
